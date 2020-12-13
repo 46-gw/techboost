90586bbef5edea1ea463a45069d838b8dcb0de3b
shiro <shiro@MacBook-Pro-2020.local>
2020-12-13 17:47:46 +0900

    02-03

--- ruby_projects/hello.rb
+++ ruby_projects/hello.rb
@@ -1,1 +1,36 @@
 puts "hello ruby !!!!!!"
+
+puts "初めてのRuby"
+
+print "Ruby"
+print "はじめました\n"
+
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+
+num_array = [1,2,3,4]
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+
+
+shiro = { name: "shiro natsuko", birth: "0227", type:"AB" }
+puts shiro[:name]
+puts shiro[:birth]
+puts shiro[:type]
+
+
+def plus_ruby(name)
+ name + "ruby"
+end
+p plus_ruby("like")
+p plus_ruby("yhee")
+
+def plus_one(number)
+ number + 1
+end
+p plus_one(4)
+p plus_one(9)
---
+++ ruby_projects/kadai_03.rb
@@ -0,0 +1,38 @@
+n = 3
+if n == 3
+  puts 'nは3です'
+end
+
+
+if n == 3
+  puts '3、4のどちらかです'
+else n == 4
+  puts '3、4のどちらかです'
+end
+
+
+if n == 3
+  puts 'nは3です'
+else n == 4
+  puts 'nは4です'
+end
+
+
+for num in 1..5 do
+  puts "こんにちは"
+end
+
+
+[1,2,3,4,5].each do |num|
+  p num
+end
+
+
+n = 2
+if n == 3
+  puts '２です'
+elsif n == 4
+  puts '３です'
+else
+  puts 'それ以外です'
+end
